5e178af240585788528127d11bf5c988df0951e9
Nathan Vegdahl <cessen@cessen.com>
2014-01-01 11:33:29 -0800

    Fixed stack-smashing bug due to grid being allowed to be too large.
    This is a weird one that I still need to investigate more, but for now
    limiting the grid size to a max of 64x64 keeps things in check.

--- config.cpp
+++ config.cpp
@@ -6,7 +6,8 @@ namespace Config
 {
 float dice_rate = 0.5; // 0.7 is about half pixel area
 float min_upoly_size = 0.001; // Approximate minimum micropolygon size in world space
-size_t max_grid_size = 120;
+size_t max_grid_size = 64; // Shouldn't be higher than 64.  TODO: investigate why, because
+// in theory it should be able to go up to 255
 float grid_cache_size = 256.0; // In MB
 
 int bucket_size = 32; // Size of the render buckets in pixels (size*size)
