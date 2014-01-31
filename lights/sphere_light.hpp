3f7af69cf5321fcd78c8905cadf82c5ea64f4b1c
Nathan Vegdahl <cessen@cessen.com>
2014-01-30 22:58:13 -0800

    Correct sphere light sampling.
    There is still a minor inaccuracy only if there are blockers inside the
    volume of the light, but that is unlikely to cause inaccuracies in
    practice for most scenes.  Nevertheless, I left a todo note to fix that.

--- lights/sphere_light.hpp
+++ lights/sphere_light.hpp
@@ -16,21 +16,35 @@ class SphereLight: public Light
 	Vec3 pos;
 	float radius;
 	Color col;
+	float surface_area_inv;
 
 public:
 	SphereLight(Vec3 pos_, float radius_, Color col_) {
 		pos = pos_;
 		radius = radius_;
+		surface_area_inv = 1.0f / (4 * M_PI * radius * radius);
 		col = col_;
 	}
 
+	/**
+	 * Samples the sphere light from a given point.
+	 *
+	 * Note that shadowing will only be 100% correct as long as there are no
+	 * blockers inside the sphere light, because the generated ray sometimes
+	 * ends inside the sphere rather than on the surface.  In general this is
+	 * unlikely to cause noticable artifacts.  But it's worth noting.
+	 *
+	 * TODO: fix the above note whenever you get around to implementing volume
+	 * rendering, as it may meaningfully impact lighting effects from a sphere
+	 * light embedded inside a volume.
+	 */
 	virtual Color sample(const Vec3 &arr, float u, float v, float time, Vec3 *shadow_vec) const {
 		// Create a coordinate system from the vector between the
 		// point and the center of the light
 		Vec3 x, y, z;
 		z = pos - arr;
-		const float d2 = z.length2();
-		const float d = std::sqrt(d2);
+		const float d2 = z.length2();  // Distance from center of sphere squared
+		const float d = std::sqrt(d2); // Distance from center of sphere
 		coordinate_system_from_vec3(z, &x, &y);
 		x.normalize();
 		y.normalize();
@@ -39,23 +53,31 @@ public:
 		// If we're outside the sphere, sample the surface based on
 		// the angle it subtends from the point being lit.
 		if (d > radius) {
-			// Sample the surface of the sphere
-			Vec3 sample = uniform_sample_sphere_from_distance(radius, d, u, v);
+			// Calculate the portion of the sphere visible from the point
+			const float sin_theta_max2 = std::min(1.0f, (radius * radius) / d2);
+			const float cos_theta_max2 = 1.0f - sin_theta_max2;
+			const float sin_theta_max = std::sqrt(sin_theta_max2);
+			const float cos_theta_max = std::sqrt(cos_theta_max2);
 
-			// Map the sample to world coordinates
-			*shadow_vec = (x * sample[0]) + (y * sample[1]) + (z * sample[2]);
+			// Calculate the solid angle the sphere takes up from the point
+			const float solid_angle = 2 * M_PI * (1.0f - cos_theta_max);
 
-			const float sd2 = shadow_vec->length2();
+			// Calculate the length that the shadow ray should be.
+			// TODO: make the length end exactly on the surface of
+			// the sphere.
+			const float disc_radius = cos_theta_max * radius;
+			const float disc_dist = d - (sin_theta_max * radius);
+			const float length = std::sqrt((disc_dist * disc_dist) + (disc_radius * disc_radius));
 
-			if (sd2 > 0)
-				return col / sd2;
-			else
-				return col; // Fudge for divide by zero.
+			// Sample the cone subtended by the sphere
+			Vec3 sample = uniform_sample_cone(u, v, cos_theta_max);
+			*shadow_vec = ((x * sample[0]) + (y * sample[1]) + (z * sample[2])).normalized() * length;
 
+			return col * solid_angle * surface_area_inv * (0.5f / M_PI);
 		} else {
-			// If we're inside the sphere, there's no light.
+			// If we're inside the sphere, there's light from every direction.
 			*shadow_vec = uniform_sample_sphere(u, v);
-			return Color(0.0f);
+			return col * surface_area_inv;
 		}
 
 	}
