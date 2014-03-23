5c047a9fbe5354e7b537edeb6ed6258249395fcc
Nathan Vegdahl <cessen@cessen.com>
2014-03-23 10:22:21 -0700

    Changed auto-formatting script to use .orig~ as the file suffix.
    Various applications recognize the trailing ~ as a temp/backup marker,
    which makes things easier to work with.

--- format_code.sh
+++ format_code.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Auto-formats all C/C++ code with Artistic Style (http://astyle.sourceforge.net/)
 # to adhere to a consistent coding style.  Should be run before committing.
-astyle --options=none -Q -A3 -tSHU  -R "*.hpp" "*.cpp" "*.h" "*.c"
+astyle --options=none --suffix=".orig~" -Q -A3 -tSHU  -R "*.hpp" "*.cpp" "*.h" "*.c"
 
